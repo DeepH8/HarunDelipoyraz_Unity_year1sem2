a4c55a24895c87dad51e572310f91aededec102b
DeepH8 <harun05delipoyraz@gmail.com>
2026-04-15 16:10:06 +0300

    UI practice
    Added canvas in new scene, title, play button

---
+++ harun_y1s2/Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Debug.Log("Menu scene opened");
+    }
+
+    public void PlayButton()
+    {
+        Debug.Log("Play button pressed.");
+        
+    }
+}
